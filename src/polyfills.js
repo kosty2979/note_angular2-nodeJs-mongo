8e544dfb9fce24e8a39a548eba8349762d2911de
kosty2979 <kosty2979@gmail.com>
2017-03-10 14:28:00 +0200

    first commit

---
+++ src/polyfills.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//<reference path="../node_modules/@types/node/index.d.ts">
+require("core-js/es6");
+require("core-js/es7/reflect");
+require('zone.js/dist/zone');
+//</reference> 
+//# sourceMappingURL=polyfills.js.map
